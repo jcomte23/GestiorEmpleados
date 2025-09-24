ac81d2873ef8262ed80601c714dd9b5fe4d85d25
Javier Cómbita Téllez <javiercombita2014@gmail.com>
2025-09-23 19:28:08 -0500

    Feat: se creo la clase empresa

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
 ﻿using GestiorEmpleados.Models;
 
+var empleado = new Empleado
+{
+    Id = Guid.NewGuid(),
+    Nombre = "Juan",
+    Apellido = "Pérez",
+    NumeroDeIdentificacion = "123456789",
+    Edad = 30,
+    Posicion = "Desarrollador",
+    Salario = 50000
+};
+
+var empleado2 = new Empleado
+{
+    Id = Guid.NewGuid(),
+    Nombre = "María",
+    Apellido = "Gómez",
+    NumeroDeIdentificacion = "987654321",
+    Edad = 28,
+    Posicion = "Diseñadora",
+    Salario = 45000
+};
